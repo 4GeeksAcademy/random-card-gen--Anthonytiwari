e466f900f14a0650ab1f84bcb27010a61c4cf85a
Anthony Tiwari <Anthonyptiwari90@gmail.com>
2025-06-23 23:02:24 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+    let randomCard = () => {
+        let number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "j", "A"];
+        let randomNumber = number[Math.floor(Math.random() * number.length)];
+
+        return randomNumber
+    }
+
+    document.getElementById("aleatorio").innerHTML = randomCard()
+
+    let prueba = () => {
+
+        let classCard = ["♥", "♠", "♣", "♦"];
+        let randomClassCard = classCard[Math.floor(Math.random() * classCard.length)];
+
+        return randomClassCard
+    }
+    let palo = prueba()
+    document.getElementById("card-palo").innerHTML = palo
+    document.getElementById("card-palo2").innerHTML = palo
+    if (palo=="♥" || palo=="♦"){
+
+        document.getElementById("card-palo2").style.color= "red" 
+    }
+     if (palo=="♥" || palo=="♦"){
+
+        document.getElementById("card-palo").style.color= "red" 
+    }
+
 };
